a2931e99c9aca98a63a33be91b3fdd3d7b812801
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-09-23 00:28:00 +0300

    - Add app_dark_colors.dart - Add assets images - Add app styles for regular, medium, and bold text - Added a new file "app_styles.dart" in the "lib\core\utils" directory - Defined font families, sizes, and colors for various text styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+
+  // Regular font weight
+  static const _regular = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  // Medium font weight
+  static const _medium = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  // Bold font weight
+  static const _bold = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+  );
+
+  // Regular styles 
+  static final TextStyle regular12 = _regular.copyWith(
+    fontSize: 12,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle regular14 = _regular.copyWith(
+    fontSize: 14,
+    color: const Color(0xFFA0AEC0), // Light gray color
+  );
+
+  static final TextStyle regular16 = _regular.copyWith(
+    fontSize: 16,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  // Medium styles
+  static final TextStyle medium10 = _medium.copyWith(
+    fontSize: 10,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle medium12 = _medium.copyWith(
+    fontSize: 12,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle medium14 = _medium.copyWith(
+    fontSize: 14,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle medium16 = _medium.copyWith(
+    fontSize: 16,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  // Bold styles
+  static final TextStyle bold10 = _bold.copyWith(
+    fontSize: 10,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle bold14 = _bold.copyWith(
+    fontSize: 14,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle bold16 = _bold.copyWith(
+    fontSize: 16,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle bold18 = _bold.copyWith(
+    fontSize: 18,
+    color: const Color(0xFFFFFFFF),
+  );
+
+  static final TextStyle bold20 = _bold.copyWith(
+    fontSize: 20,
+    color: const Color(0xFFFFFFFF),
+  );
+}
