c7747bae212b36d0f90ba469ba7a2774f5250486
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-09-24 13:03:28 +0300

    - add dashcubit

--- lib/features/dashboard/ui/views/dashboard_view.dart
+++ lib/features/dashboard/ui/views/dashboard_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
-import '../../../../core/utils/app_images.dart';
-import '../../../../core/widgets/custom_drawer.dart';
 import '../widgets/dashboard_view_body.dart';
 
 class DashboardView extends StatelessWidget {
@@ -10,24 +7,6 @@ class DashboardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              Assets.assetsImagesDashboardBackground,
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: const Row(
-          children: [
-            CustomDrawer(),
-            Gap(24),
-            DashboardViewBody(),
-          ],
-        ),
-      ),
-    );
+    return const DashboardViewBody();
   }
 }
